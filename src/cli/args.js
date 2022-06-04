7e4687a043f28b380bd141589989ac68b8d7f9f3
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-04 20:16:23 +0300

    fix: File system => env to args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
+const { env } = process;
+
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const envName = "RSS_USER_NAME";
+  const envSurname = "RSS_USER_SURNAME";
+  console.log(`${envName} is ${env[envName]}, ${envSurname} is ${env[envSurname]}`);
 };
+
+export default (() => {
+  parseArgs();
+})();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,12 +1,3 @@
-const { env } = process;
-
 export const parseEnv = () => {
-  // Write your code here
-  const envName = "RSS_USER_NAME";
-  const envSurname = "RSS_USER_SURNAME";
-  console.log(`${envName}=${env[envName]}; ${envSurname}=${env[envSurname]}`);
+    // Write your code here 
 };
-
-export default (() => {
-  parseEnv();
-})();
