68c7b323441037bbac3c2a0680da63de783f9242
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-04 18:59:40 +0300

    feat: File system => list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import { existsSync, mkdirSync, readdirSync, copyFileSync } from "node:fs";
+
+const basePath = "src/fs";
+const folderPath = `${basePath}/files`;
+
 export const list = async () => {
-    // Write your code here 
+  // Write your code here
+  if (!existsSync(folderPath)) {
+    console.error("FS operation failed");
+  } else {
+    try {
+      const fileList = await readdirSync(folderPath);
+      console.log(`file list => ${fileList}`);
+    } catch (error) {
+      console.error("FS operation failed");
+    }
+  }
 };
+
+export default (() => {
+  list();
+})();
