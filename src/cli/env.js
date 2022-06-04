684e42e8fb47cda8d3cc6e056a16c9707da636a1
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-04 20:44:18 +0300

    refactoring: env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
+const { env } = process;
+
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envName = "RSS_USER_NAME";
+  const envSurname = "RSS_USER_SURNAME";
+  console.log(`${envName}=${env[envName]}; ${envSurname}=${env[envSurname]}`);
 };
+
+export default (() => {
+  parseEnv();
+})();
