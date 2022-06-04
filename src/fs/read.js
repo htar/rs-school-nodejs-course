417e9f1a09bc3139095d640fb76d5ea5d125c25e
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-04 19:05:03 +0300

    feat: File system => read done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { readFileSync } from "node:fs";
+
+const filePath = "src/fs/files/fileToRead.txt";
+const fileContent = "I am fresh and young";
+
 export const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const data = await readFileSync(filePath, { encoding: "utf8", flag: "r" });
+    console.log(data);
+  } catch (error) {
+    console.error("FS operation failed");
+  }
 };
+
+export default (() => {
+  read();
+})();
