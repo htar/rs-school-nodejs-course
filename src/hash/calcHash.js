583f11aefb9b20fc9c090864fe155d5897faf7d6
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-04 21:14:17 +0300

    feat: File system =>  calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import crypto from "crypto";
+const hash = crypto.getHashes();
+
+export const calculateHash = () => {
+  // Write your code here
+  const hex = crypto.createHash("sha256").digest("hex");
+  console.log("hex", hex);
+  return hex;
 };
+
+export default (() => {
+  calculateHash();
+})();
