999c019ee0f381651e22b6ad11243279c63167c3
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-05 18:04:20 +0300

    feat: Steams => write done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,20 @@
+import { createWriteStream } from "fs";
+import { stdin } from "process";
+
 export const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const basePath = "src/streams";
+  const folderPath = `${basePath}/files`;
+
+  const stream = createWriteStream(`${folderPath}/fileToWrite.txt`, {
+    encoding: "utf8",
+  });
+
+  stdin.pipe(stream);
+  stream.write("hello, ");
+  stream.end("world!");
 };
+
+export default (() => {
+  write();
+})();
