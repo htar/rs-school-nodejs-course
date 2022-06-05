4a32b3ee5b22721a797bfaad81addc41c48b3371
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-05 19:17:59 +0300

    feat: Wt => worker done

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,22 @@
+import { parentPort, workerData } from "worker_threads";
+
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) => {
+  return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+};
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+
+  try {
+    const res = nthFibonacci(workerData);
+
+    parentPort.postMessage(res);
+  } catch (error) {
+    console.error("wt worker error", error);
+  }
 };
+
+export default (() => {
+    sendResult();
+})();
