3cdf97d049694342c54f9e9f51e62463b240520f
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-05 17:27:38 +0300

    fix: package.json update scripts name

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,7 +6,7 @@ const fileContent = "I am fresh and young";
 export const create = async () => {
   // Write your code here
   try {
-    const file = await readFileSync(filePath, (err, data) => {});
+    const file = await readFileSync(filePath, { encoding: "utf8" });
     if (!!file) console.error("FS operation failed");
   } catch (error) {
     await writeFileSync(filePath, fileContent, { overwrite: false });
